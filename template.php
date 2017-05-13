964834ba4c0adb4b20955a0b8a37b973d616a9cc
krisa <krisa@krisa.cz>
2017-05-13 13:34:19 +0200

    Buttons switched from display:none to class=“disabled”

--- template.php
+++ template.php
@@ -24,12 +24,12 @@ if(!empty($field->options())) {
 	<div class="tablex-ctrl">
 		<div class="row-ctrl filling-block"></div>
 		<div class="row-cell column-ctrl" v-for="n in columnCount">
-			<i class="fa fa-chevron-left" v-show="n !== 1" v-on:click="moveColumn(n-1, 'left')"></i>
-			<i class="fa fa-times-circle" v-show="columnCount > options.minColumns" v-on:click="deleteColumn(n)"></i>
-			<i class="fa fa-chevron-right" v-show="n !== columnCount"  v-on:click="moveColumn(n-1, 'right')"></i>
+			<i class="fa fa-chevron-left"  v-bind:class="{ disabled: n == 1 }" v-on:click="moveColumn(n-1, 'left')"></i>
+			<i class="fa fa-times-circle"  v-bind:class="{ disabled: columnCount <= options.minColumns }" v-on:click="deleteColumn(n)"></i>
+			<i class="fa fa-chevron-right" v-bind:class="{ disabled: n == columnCount }"  v-on:click="moveColumn(n-1, 'right')"></i>
 		</div>
 		<div class="row-ctrl add-column">
-			<i class="fa fa-plus" v-show="columnCount < options.maxColumns" v-on:click="addColumn()"></i>
+			<i class="fa fa-plus" v-bind:class="{ disabled: columnCount >= options.maxColumns }" v-on:click="addColumn()"></i>
 		</div>
 	</div>
 
@@ -46,8 +46,8 @@ if(!empty($field->options())) {
 		<!-- Row -->
 		<div class="tablex-row" v-for="(row, rowIndex) in tables">
 			<div class="row-ctrl move-row">
-				<i class="fa fa-chevron-up"  v-show="rowIndex !== 0" v-on:click="moveRow(rowIndex, 'up')"></i>
-				<i class="fa fa-chevron-down" v-show="rowIndex !== rowCount-1"v-on:click="moveRow(rowIndex,	'down')"></i>
+				<i class="fa fa-chevron-up" v-bind:class="{ disabled: rowIndex == 0 }" v-on:click="moveRow(rowIndex, 'up')"></i>
+				<i class="fa fa-chevron-down" v-bind:class="{ disabled: rowIndex == rowCount-1 }" v-on:click="moveRow(rowIndex,	'down')"></i>
 			</div>
 			<input class="row-cell input" type="text" :name="'<?= $field->name() ?>[tables]['+ rowIndex +']'"  v-model="row[cellIndex]"  v-for="(cell, cellIndex) in row">
 			<div class="row-ctrl delete-row">
